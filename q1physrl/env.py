d5e55162979188104a065acd9546fcb196efaa18
Matthew Earl <git@matthewearl.com>
2020-01-29 18:35:30 +0000

    Add yawing as an action

--- q1physrl/env.py
+++ q1physrl/env.py
@@ -20,7 +20,7 @@ _TIME_DELTA = np.float32(0.014)
 _FMOVE_MAX = np.float32(800)  # units per second
 _SMOVE_MAX = np.float32(700)  # units per second
 _YAW_SPEED = np.float32(360)  # degrees per second
-_INITIAL_STATE = {'z_pos': np.float32(32.82),
+_INITIAL_STATE = {'z_pos': np.float32(32.843201),
                   'vel': np.array([0, 0, -12], dtype=np.float32),
                   'on_ground': np.bool(False),
                   'jump_released': np.bool(True)}
@@ -34,23 +34,35 @@ class PhysEnv(ray.rllib.env.VectorEnv):
     _time: np.ndarray
     _step_num: int
 
+
+    def _round_vel(self, v):
+        # See sv_main.c : SV_WriteClientdataToMessage
+        return (v / 16).astype(int) * 16
+
+    def _round_origin(self, o):
+        # See common.c : MSG_WriteCoord
+
+        # We should not send a changed origin if the position has not changed by more than 0.1.
+        # See `miss` variable in `SV_WriteEntitiesToClient`.
+        return np.round(o * 8) / 8
+
     def _get_obs(self):
         ps = self._player_state
 
-        # As per the network protocol, velocity is quantized to 16 ups, and position is quantized to
-        # 0.125 units.
-        vel = (ps.vel / 16).astype(int) * 16  
-        z_pos = (ps.z_pos * 8).astype(int) / 8
+        vel = self._round_vel(ps.vel)
+        z_pos = self._round_origin(ps.z_pos)
 
         return np.concatenate([self._yaw[:, None], z_pos[:, None], vel], axis=1)
 
     def _get_obs_at(self, index):
         ps = self._player_state
-        return np.concatenate([self._yaw[index][None], ps.z_pos[index][None], ps.vel[index]], axis=0)
+        z_pos = self._round_origin(ps.z_pos[index])
+        vel = self._round_vel(ps.vel[index])
+        return np.concatenate([self._yaw[index][None], z_pos[None], vel], axis=0)
 
     def __init__(self, config):
         self.num_envs = config['num_envs']
-        self.action_space = gym.spaces.MultiDiscrete([3, 2])
+        self.action_space = gym.spaces.MultiDiscrete([3, 3, 2])
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf,
                                                 shape=(5,), dtype=np.float32)
         self._step_num = 0
@@ -76,17 +88,16 @@ class PhysEnv(ray.rllib.env.VectorEnv):
     def vector_step(self, actions):
         actions = np.stack(actions)
 
-        #next_yaw = self._yaw + _TIME_DELTA * _YAW_SPEED * (actions[:, 0] - 1)
-        next_yaw = self._yaw
-        smove = _SMOVE_MAX * (actions[:, 0] - 1)
-        fmove = _FMOVE_MAX * actions[:, 1]
+        self._yaw = self._yaw + _TIME_DELTA * _YAW_SPEED * (actions[:, 0] - 1)
+        smove = _SMOVE_MAX * (actions[:, 1] - 1)
+        fmove = _FMOVE_MAX * actions[:, 2]
 
         pitch = np.zeros((self.num_envs,), dtype=np.float32)
         roll = np.zeros((self.num_envs,), dtype=np.float32)
         button2 = np.zeros((self.num_envs,), dtype=np.bool)
         time_delta = np.full((self.num_envs,), _TIME_DELTA)
         
-        inputs = phys.Inputs(yaw=next_yaw, pitch=pitch, roll=roll, fmove=fmove, smove=smove,
+        inputs = phys.Inputs(yaw=self._yaw, pitch=pitch, roll=roll, fmove=fmove, smove=smove,
                              button2=button2, time_delta=time_delta)
 
         vel_before = self._player_state.vel[:, 1]
@@ -106,16 +117,17 @@ class PhysEnv(ray.rllib.env.VectorEnv):
 
 
 def _make_observation(client):
-    yaw = client.angles[1]
+    yaw = 180 * client.angles[1] / np.pi
     vel = np.array(client.velocity)
     z_pos = client.player_origin[2]
     return np.concatenate([[yaw], [z_pos], vel])
 
 
 def _apply_action(client, action):
-    yaw = client.angles[1]
-    smove = int(_SMOVE_MAX * (action[0] - 1))
-    fmove = int(_FMOVE_MAX * action[1])
+    yaw = (180 * client.angles[1] / np.pi) + _TIME_DELTA * _YAW_SPEED * (action[0] - 1)
+    yaw *= np.pi / 180
+    smove = int(_SMOVE_MAX * (action[1] - 1))
+    fmove = int(_FMOVE_MAX * action[2])
     client.move(pitch=0, yaw=yaw, roll=0, forward=fmove, side=smove,
                 up=0, buttons=0, impulse=0)
 
@@ -129,6 +141,8 @@ async def eval_coro(port, trainer, demo_fname):
     try:
         demo = client.record_demo()
         await client.wait_until_spawn()
+        client.move(*client.angles, 0, 0, 0, 0, 0)
+        await client.wait_for_movement(client.view_entity)
         for _ in range(358):
             obs = _make_observation(client)
             obs_list.append(obs)
