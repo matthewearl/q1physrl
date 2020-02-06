8e6373b495657a8300c8cb5dde156e975b70a6e4
Matthew Earl <git@matthewearl.com>
2020-02-06 21:06:36 +0000

    add a separate list of stats to print, and not track the maximum of

--- q1physrl/train.py
+++ q1physrl/train.py
@@ -10,7 +10,6 @@ import q1physrl.env
 
 def make_trainer():
     return ray.rllib.agents.ppo.PPOTrainer(
-        # trainer = ray.rllib.agents.a3c.A2CTrainer(
         env=q1physrl.env.PhysEnv,
         config={"env_config": {"num_envs": 100}, "gamma": 0.99}
     )
@@ -20,6 +19,16 @@ _STATS_TO_TRACK = [
     'episode_reward_mean',
     'episode_reward_max'
 ]
+
+_STATS_TO_PRINT = _STATS_TO_TRACK #+ ['info/learner/default_policy/entropy_coeff']
+
+
+def _get_stat(stats, k):
+    parts = k.split('/')
+    x = stats
+    for part in parts:
+        x = x[part]
+    return x
 
 
 @dataclasses.dataclass
@@ -38,12 +47,12 @@ def train():
     i = 0
     while True:
         stats = trainer.train()
-        print('Iteration:', i, 'Current:', {k: stats[k] for k in _STATS_TO_TRACK})
+        print('Iteration:', i, 'Current:', {k: _get_stat(stats, k) for k in _STATS_TO_PRINT})
 
         # Work out which (if any) stats just exceeded the previous best value.
         stats_to_save = []
         for k in _STATS_TO_TRACK:
-            if not np.isnan(stats[k]) and (k not in best_stats or stats[k] > best_stats[k].val):
+            if not np.isnan(_get_stat(stats, k)) and (k not in best_stats or _get_stat(stats, k) > best_stats[k].val):
                 stats_to_save.append(k)
 
         # Make a checkpoint whenever one of the stats exceeds its previous best,
@@ -51,8 +60,8 @@ def train():
         if i % 100 == 0 or stats_to_save:
             fname = trainer.save()
             for k in stats_to_save:
-                if not np.isnan(stats[k]):
-                    best_stats[k] = _Stat(stats[k], fname)
+                if not np.isnan(_get_stat(stats, k)):
+                    best_stats[k] = _Stat(_get_stat(stats, k), fname)
             print('Best:')
             pprint(best_stats)
         i += 1
